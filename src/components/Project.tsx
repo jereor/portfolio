d479d7c146df4a05f604223ccb13c0a3379efe7d
jereor <jere.orava@hotmail.com>
2026-03-16 10:53:29 +0200

    Add Warframe to the Projects section

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,17 +1,38 @@
 import React from "react";
-import thumbnail1 from '../assets/images/asset-tools-thumbnail.png';
+import warframeOfferHighlightThumbnail from '../assets/images/warframe/offer_highlight/offer_highlight_design.png';
+import warframeBreadcrumbingThumbnail from '../assets/images/warframe/breadcrumbing/breadcrumbing_pause_arsenal.png';
+import assetToolsThumbnail from '../assets/images/asset-tools-thumbnail.png';
 import '../assets/styles/Project.scss';
 
 function Project() {
     return(
     <div className="projects-container" id="projects">
+        <h1>Work Projects</h1>
+        <div className="projects-grid">
+            <div className="project">
+                <a href="https://play.google.com/store/apps/details?id=com.digitalextremes.warframemobile" target="_blank" rel="noreferrer"><h2>Warframe - In-App Store Offer Highlights</h2></a>
+                <a href="https://play.google.com/store/apps/details?id=com.digitalextremes.warframemobile" target="_blank" rel="noreferrer"><img src={warframeOfferHighlightThumbnail} className="zoom" alt="thumbnail" width="100%"/></a>
+                <p>Designed and implemented a C++ system to surface curated in‑game offers whenever players opened the pause menu.</p>
+                <p>Implemented algorithms to prioritize limited‑time offers and high‑value premium bundles.</p>
+                <p>Streamlined the purchase flow by deep‑linking directly to the purchase screen.</p>
+                <p>Resulted in <b>~10% increase in conversion and revenue.</b></p>
+            </div>
+            <div className="project">
+                <a href="https://play.google.com/store/apps/details?id=com.digitalextremes.warframemobile" target="_blank" rel="noreferrer"><h2>Warframe - Menu Breadcrumbing System</h2></a>
+                <a href="https://play.google.com/store/apps/details?id=com.digitalextremes.warframemobile" target="_blank" rel="noreferrer"><img src={warframeBreadcrumbingThumbnail} className="zoom" alt="thumbnail" width="100%"/></a>
+                <p>Implemented a UI guidance system to help players discover newly unlocked quests, equipment, and upgrades.</p>
+                <p>Improved early‑game onboarding and reduced missed progression opportunities.</p>
+                <p><b>Increased player retention and overall satisfaction with the progression loop.</b></p>
+                <p>Well‑received by both new players and long‑time Warframe veterans.</p>
+            </div>
+        </div>
+
         <h1>Personal Projects</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://github.com/jereor/asset-tools/" target="_blank" rel="noreferrer"><img src={thumbnail1} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/jereor/asset-tools/" target="_blank" rel="noreferrer"><h2>Asset Tools</h2></a>
-                <p>Developed a C++ prototype of an internal game development tool designed to support content creation workflows and asset pipelines.</p>
-                <p>The project focuses on pre-engine asset validation similar to what game companies like Supercell would use. The idea is that assets are validated close to asset creation to catch problems before the assets are imported to the game engine. First by artists and designers, and later re-validated by build pipelines.</p>
+                <a href="https://github.com/jereor/asset-tools/" target="_blank" rel="noreferrer"><h2>Asset Validation Tool</h2></a>
+                <a href="https://github.com/jereor/asset-tools/" target="_blank" rel="noreferrer"><img src={assetToolsThumbnail} className="zoom" alt="thumbnail" width="100%"/></a>
+                <p>Developed a C++ prototype of an internal asset validation tool designed to support content creation workflows and asset pipelines. This project is built with CMake and integrates effortlessly into Jenkins CI using Docker.</p>
             </div>
         </div>
     </div>
